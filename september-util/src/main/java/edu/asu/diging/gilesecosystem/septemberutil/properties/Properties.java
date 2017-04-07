6525972db3b6ff8226784c5807339aaae5905197
Suraj Nilapwar <suraj.nilapwar@asu.edu>
2017-04-07 11:04:51 -0700

    [GECO-46] new branch

---
+++ september-util/src/main/java/edu/asu/diging/gilesecosystem/septemberutil/properties/Properties.java
@@ -0,0 +1,5 @@
+package edu.asu.diging.gilesecosystem.septemberutil.properties;
+
+public interface Properties {
+    public final static String KAFKA_TOPIC_SYSTEM_MESSAGES = "topic_system_messages";
+}
---
+++ september-util/src/main/java/edu/asu/diging/gilesecosystem/septemberutil/service/ISystemMessageHandler.java
@@ -0,0 +1,8 @@
+package edu.asu.diging.gilesecosystem.septemberutil.service;
+
+public interface ISystemMessageHandler {
+
+    public abstract void handleError(String msg, Exception exception);
+
+}
+
